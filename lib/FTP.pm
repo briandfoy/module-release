d98e56beb93ad0469d1fc962e67f1c9f2d2938eb
brian d foy <brian.d.foy@gmail.com>
2008-07-30 14:56:09 +0000

    * Finish testing and refactoring the FTP stuff
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@2615 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/FTP.pm
+++ lib/FTP.pm
@@ -5,7 +5,12 @@ use strict;
 use warnings;
 use base qw(Exporter);
 
-our @EXPORT = qw(ftp_upload);
+our @EXPORT = qw( 
+	ftp_upload ftp_passive_on ftp_passive_off ftp_passive 
+	ftp_class_name get_ftp_object 
+	default_ftp_hostname default_ftp_user 
+	default_ftp_password default_ftp_upload_dir
+	);
 
 our $VERSION = '0.10_02';
 
@@ -24,64 +29,60 @@ to upload a file
 
 =item ftp_upload( PARAMS )
 
-Upload the files to the FTP servers.
+Upload the file in C<local_file> as C<remote_file> to the FTP server. You
+can pass parameters to ftp_upload to change some of the behavior. Each
+parameter has a default value (see the default_* subs at the end of the
+module):
 
-	local_file
-	remote_file
-	upload_dir
-	user
-	password
-	hostname
+	Input key       Default value
+	----------      -------------
+	upload_dir      /incoming
+	user            anonymous
+	password        joe@example.com
+	hostname        pause.perl.org
 
 =cut
 
 sub ftp_upload
 	{
-	require Net::FTP;
+	my $self = shift;
+	
+	my %defaults = map { my $m = "default_ftp_$_"; $_, $self->$m() } qw(
+		upload_dir
+		user
+		password
+		hostname
+		);
+	
+	my %params = ( %defaults, @_ );
 
-	my( $self, %params ) = @_;
+	$self->_print( "Logging in to $params{hostname}\n" );
+		
+	my $ftp = $self->get_ftp_object( $params{hostname} );
+	
+	$ftp->login( @params{ qw(user password) } )
+		or $self->_die( "Couldn't log in to $params{hostname}" );
 
-	( $self->{release} ) = $self->remote_file =~ m/^(.*?)(?:\.tar\.gz)?$/g;
+	$ftp->binary;
 
-	my $config = $self->config;
+	$ftp->cwd( $params{'upload_dir'} )
+		or $self->_die( "Couldn't chdir to $params{'upload_dir'}" );
 
-	$self->_print( "Release name is $self->{release}\n" );
-	$self->_debug( "Will use passive FTP transfers\n" ) if $self->passive_ftp;
+	$self->_print( "Putting " . $self->local_file . "\n" );
+	my $remote_file = $ftp->put( $self->local_file, $self->remote_file );
+	$self->_die( "PUT failed: " . $ftp->message . "\n" )
+		if $remote_file ne $self->remote_file;
 
-	my $local_file = $self->local_file;
-	my $local_size = -s $local_file;
+	my $remote_size = $ftp->size( $self->remote_file );
 
-	foreach my $site ( $params{hostname} )
-		{
-		$self->_print( "Logging in to $site\n" );
-		my $ftp = Net::FTP->new(
-			$site,
-			Hash    => \*STDOUT,
-			Debug   => $self->debug,
-			Passive => $self->passive_ftp
-			) or $self->_die( "Couldn't open FTP connection to $site: $@" );
+	no warnings 'uninitialized';
+	$self->_print( "WARNING: Uploaded file is $remote_size bytes, " .
+		"but local file is " . -s $self->local_file . " bytes" )
+			if $remote_size != -s $self->local_file;
 
-		$ftp->login( @params{ qw(user password) } )
-			or $self->_die( "Couldn't log in anonymously to $site" );
+	$ftp->quit;
 
-		$ftp->binary;
-
-		$ftp->cwd( $params{'upload_dir'} )
-			or $self->_die( "Couldn't chdir to $params{'upload_dir'}" );
-
-		$self->_print( "Putting $local_file\n" );
-		my $remote_file = $ftp->put( $self->local_file, $self->remote_file );
-		$self->_die( "PUT failed: " . $ftp->message . "\n" )
-			if $remote_file ne $self->remote_file;
-
-		my $remote_size = $ftp->size( $self->remote_file );
-
-		$self->_print( "WARNING: Uploaded file is $remote_size bytes, " .
-			"but local file is $local_size bytes" )
-				if $remote_size != $local_size;
-
-		$ftp->quit;
-		}
+	$self->_print( "File uploaded\n" );
 	}
 
 =item ftp_passive_on
@@ -103,6 +104,80 @@ sub ftp_passive_on  { $_[0]->{ftp_passive} = 1 }
 sub ftp_passive_off { $_[0]->{ftp_passive} = 0 }
 
 sub ftp_passive     { $_[0]->{ftp_passive} }
+
+=item ftp_class_name
+
+The class name to use to create the FTP object. The class needs to follow
+the C<Net::FTP> interface.
+
+=cut
+
+sub ftp_class_name { 'Net::FTP' }
+
+=item get_ftp_object( HOSTNAME )
+
+Create and returnt the FTP object, based on the class name from 
+C<ftp_class_name>. IT connects to HOSTNAME, but does not login.
+
+=cut
+
+sub get_ftp_object 
+	{
+	my( $self, $site ) = @_;
+	
+	my $class = $self->ftp_class_name;
+	my $rc = eval { eval "require $class" };
+	
+	$self->_die( "Couldn't load $class: $@" ) unless defined $rc;
+		
+	$self->_debug( "Will use passive FTP transfers\n" ) if $self->ftp_passive;
+
+	my $ftp = $class->new(
+		$site,
+		Hash    => \*STDOUT,
+		Debug   => $self->debug,
+		Passive => $self->ftp_passive,
+		) or $self->_die( "Couldn't open FTP connection to $site: $@" );
+	
+	return $ftp;
+	}
+
+=back
+
+=head2 Default values
+
+Override these methods to change the default values. Remember that 
+the overridden methods have to show up in the C<Module::Release>
+namespace.
+
+=over 4
+
+=item default_ftp_hostname
+
+pause.perl.org
+
+=item default_ftp_user
+
+anonymous
+
+=item default_ftp_password
+
+joe@example.com
+
+=item default_ftp_upload_dir
+
+/incoming
+
+=cut
+
+sub default_ftp_hostname   { "pause.perl.org" }
+
+sub default_ftp_user       { "anonymous" }
+
+sub default_ftp_password   { "joe\@example.com" }
+
+sub default_ftp_upload_dir { "/incoming" }
+
 
 =back
 
