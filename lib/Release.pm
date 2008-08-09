f52764eee0a08b5b96d4ca3bda67f0de3d4ff947
brian d foy <brian.d.foy@gmail.com>
2008-08-09 08:27:01 +0000

    * don't die if a command returns something true in $?. The git command returns 255 although it works. Dumb git!
    Now just _warn instead and let the caller decide what to do
    based on the command output.
    
    
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@2642 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Release.pm
+++ lib/Release.pm
@@ -25,7 +25,7 @@ no warnings;
 
 #$VERSION = sprintf "%d.%02d", qw( 1 26 );
 
-$VERSION = 2.00_01;
+$VERSION = '2.00_01';
 
 use Carp;
 use File::Spec;
@@ -1236,7 +1236,7 @@ sub run
 
 	my $readlen = $self->debug ? 1 : 256;
 
-	while (read $fh, $buffer, $readlen)
+	while( read $fh, $buffer, $readlen )
 		{
 		$output .= $_;
 		$self->_debug( $_, $buffer );
@@ -1248,7 +1248,7 @@ sub run
 	unless( close $fh )
 		{
 		$self->_run_error_set;
-		$self->_die(  "Command [$command] didn't close cleanly" );
+		$self->_warn(  "Command [$command] didn't close cleanly: $?" );
 		}
 
 	return $output;
