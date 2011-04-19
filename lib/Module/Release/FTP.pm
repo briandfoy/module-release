effd0f9469a4d0789aa74555f816b3a8855e210b
brian d foy <brian.d.foy@gmail.com>
2011-04-19 11:06:04 +0200

    Return 1 in the string eval to ensure we have a true value
    It's probably excessive, but I like doing it as a rule.

--- lib/Module/Release/FTP.pm
+++ lib/Module/Release/FTP.pm
@@ -129,7 +129,7 @@ sub get_ftp_object
 	my( $self, $site ) = @_;
 
 	my $class = $self->ftp_class_name;
-	my $rc = eval { eval "require $class" };
+	my $rc = eval "require $class; 1";
 
 	$self->_die( "Couldn't load $class: $@" ) unless defined $rc;
 
