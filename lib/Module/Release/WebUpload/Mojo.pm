06f50d324fc6c7fd3e9ebe4e9037b0b409d244ad
brian d foy <brian.d.foy@gmail.com>
2019-12-19 08:25:59 -0500

    whitespace

--- lib/Module/Release/WebUpload/Mojo.pm
+++ lib/Module/Release/WebUpload/Mojo.pm
@@ -79,7 +79,7 @@ sub make_agent {
 	my( $self ) = @_;
 	my $agent = Mojo::UserAgent->new;
 	$agent->transactor->name( 'release' );
-	$agent->http_proxy( $self->config->http_proxy ) if $self->config->http_proxy;
+	$agent->http_proxy( $self->config->http_proxy )   if $self->config->http_proxy;
 	$agent->https_proxy( $self->config->https_proxy ) if $self->config->https_proxy;
 
 	return $agent;
