0e54ef6e97ef593af9c1b8b6de9a2c71d46f965b
brian d foy <brian.d.foy@gmail.com>
2021-01-29 20:06:50 -0500

    Add a MetaCPAN plugin

---
+++ lib/Module/Release/MetaCPAN.pm
@@ -0,0 +1,83 @@
+use v5.16;
+
+package Module::Release::MetaCPAN;
+
+use strict;
+use warnings;
+use Exporter qw(import);
+
+use Carp;
+
+our @EXPORT = qw( cpan_version );
+
+our $VERSION = '2.126_01';
+
+=encoding utf8
+
+=head1 NAME
+
+Module::Release::MetaCPAN - Interact with MetaCPAN
+
+=head1 SYNOPSIS
+
+The release script automatically loads this module if it sees a
+F<.svn> directory. The module exports C<cpan_version>.
+
+=head1 DESCRIPTION
+
+C<Module::Release::MetaCPAN> is a plugin for C<Module::Release>.
+
+These methods are B<automatically> exported in to the callers namespace
+using Exporter. You should only use it from C<Module::Release> or its
+subclasses.
+
+
+=cut
+
+=over 4
+
+=item C<cpan_version()>
+
+=cut
+
+sub _metacpan {
+	state $rc = require MetaCPAN::Client;
+	state $mcpan = MetaCPAN::Client->new;
+
+	$mcpan;
+	}
+
+
+sub cpan_version {
+	my $module = _metacpan()->module( _module() );
+
+	my $date    = $module->{data}{date};
+	my $version = $module->{data}{version};
+	}
+
+=back
+
+=head1 SEE ALSO
+
+L<Module::Release>
+
+=head1 SOURCE AVAILABILITY
+
+This source is in GitHub
+
+	https://github.com/briandfoy/module-release
+
+=head1 AUTHOR
+
+brian d foy, C<< <bdfoy@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2021, brian d foy C<< <bdfoy@cpan.org> >>. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the Artistic License 2.0.
+
+=cut
+
+1;
